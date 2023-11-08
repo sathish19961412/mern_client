b8bd13f48b809fbdfc0caa07952a62aae194879c
sathish19961412 <sathishfreelanc5@gmail.com>
2023-11-08 16:40:32 +0530

    completed to the UI Design

--- src/CreateUser.jsx
+++ src/CreateUser.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 function CreateUser() {
+
+  const [name,setName]=useState()
+  const [email,setEmail]=useState()
+  const [age,setAge]=useState()
+  const navigate=useNavigate()
+  const Submit=(e)=>{
+      e.preventDefault();
+      axios.post('http://localhost:3001/createUser',{name,email,age})
+      .then(result=>{
+        console.log(result)
+        navigate('/')
+      })
+      .catch(err=>console.log(err))
+  }
   return (
     <>
         <section>
@@ -9,19 +25,22 @@ function CreateUser() {
                      <div className='col-md-12'>
                             <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
                                 <div className='w-50 bg-white round p-3'>
-                                     <form>
+                                     <form onSubmit={Submit}>
                                         <h2>Add Users</h2>
                                         <div className='mb-2'>
                                             <label htmlFor=''>Name</label>
-                                            <input type="text" placeholder='Enter Name' className='form-control'/>
+                                            <input type="text" placeholder='Enter Name' className='form-control'
+                                            onChange={(e)=>setName(e.target.value)}/>
                                         </div>
                                         <div className='mb-2'>
                                             <label htmlFor=''>Email</label>
-                                            <input type="email" placeholder='Enter Email' className='form-control'/>
+                                            <input type="email" placeholder='Enter Email' className='form-control'
+                                            onChange={(e)=>setEmail(e.target.value)}/>
                                         </div>
                                         <div className='mb-2'>
                                             <label htmlFor=''>Age</label>
-                                            <input type="text" placeholder='Enter Age' className='form-control'/>
+                                            <input type="text" placeholder='Enter Age' className='form-control'
+                                            onChange={(e)=>setAge(e.target.value)}/>
                                         </div>
                                         <button className='btn btn-success'>Submit</button>
                                      </form>
