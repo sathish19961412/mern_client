06b03ef3824bf0e4838c4aaad3ddf6ea52647455
sathish19961412 <sathishfreelanc5@gmail.com>
2023-11-08 12:24:31 +0530

    create components to the jsx create,update,delete file

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Users from './Users'
+import CreateUser from './CreateUser'
+import UpdateUser from './UpdateUser'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section>
+          <div className='container'>
+            <div className='row'>
+                <div className='col-md-12'>
+                    <BrowserRouter>
+                       <Routes>
+                          <Route path='/' element={<Users/>} ></Route>
+                          <Route path='/create' element={<CreateUser/>} ></Route>
+                          <Route path='/update' element={<UpdateUser/>}></Route>
+                       </Routes>
+                    </BrowserRouter>
+                </div>
+            </div>
+          </div>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/Users.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+function Users() {
+    const [users,setUsers]=useState([{
+        Name:"sathish",
+        Email:"sathishfreelanc5@gmail.com",
+        Age:26
+    }])
+  return (
+    <>
+        <section>
+            <div className='container'>
+                 <div className='row'>
+                     <div className='col-md-12'>
+                            <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+                                <div className='bg-white round p-3'>
+                                    <Link to="/create" className='btn btn-success'>Add +</Link>
+                                    <table className='table'>
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Email</th>
+                                                <th>Age</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {
+                                                users.map((user)=>{
+                                                return <tr>
+                                                        <td>{user.Name}</td>
+                                                        <td>{user.Email}</td>
+                                                        <td>{user.Age}</td>
+                                                        <td>
+                                                            <button className='btn btn-success'>EDIT</button><button className='btn btn-danger'>DELETE</button>
+                                                        </td>
+                                                    </tr>
+                                                })
+                                           
+                                            }
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                     </div>
+                 </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Users;
